0abf021be02851583378f62aaff9ad58a7db85cf
linddev0209 <lindseyhildebrand0209@gmail.com>
2023-09-29 13:05:57 -0300

    pagination user auth and so on

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,12 @@ Route::resource('manager_user', UserController::class)
     ->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
+    Route::post('/user_authorization_update/{employee_id}', [UserController::class, 'update']);
+    Route::post('/user_info_update/{employee_id}', [UserController::class, 'updateInfo']);
+    Route::delete('/user_info_delete/{employee_id}', [UserController::class, 'deleteUserInfo']);
+});
+
+Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
